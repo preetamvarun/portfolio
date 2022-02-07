0b34a897ad669938298d9719e78144aada9163f2
Preetam Varun <preetamarun99@gmail.com>
2022-02-07 10:33:01 +0530

    add typewriting effect

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
 let backgroundDivs = document.getElementsByClassName('background--noAnimate');
+let typeWriter = document.getElementById('typeWriter');
+let texts = ['Poorna Sandeep','a Web Developer','a competetive programmer'];
+let count = 0, index = 0, currentText = "", letter = "";
+
+/* IIFE (USING A SELF INVOKED FUNCTION) */
+
+(function type(){
+    if(count === texts.length){
+        count = 0;
+    } else{
+        currentText = texts[count];
+        index++;
+        letter = currentText.slice(0,index);
+        typeWriter.textContent = letter;
+        if(letter.length === currentText.length){
+            count++;
+            index = 0;
+        }
+    }
+    setTimeout(type,200);
+})();
 
 backgroundDivs = Array.from(backgroundDivs);
 
