c25656d5c5da3d4290431a9ef65f0bf35f9faee1
Preetam Varun <preetamarun99@gmail.com>
2022-02-07 13:15:44 +0530

    add typewriting effect in about section also

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let backgroundDivs = document.getElementsByClassName('background--noAnimate');
 let typeWriter = document.getElementById('typeWriter');
+let typeWriter1 = document.getElementById('typeWriter1');
 let texts = ['Poorna Sandeep','a Web Developer','a competetive programmer'];
+let texts1 = ['Web Developer','Competetive Programmer'];
 let count = 0, index = 0, currentText = "", letter = "";
-
-/* IIFE (USING A SELF INVOKED FUNCTION) */
+let count1 = 0, index1 = 0, currentText1 = "", letter1 = "";
 
 (function type(){
     if(count === texts.length){
@@ -19,7 +20,24 @@ let count = 0, index = 0, currentText = "", letter = "";
         }
     }
     setTimeout(type,200);
-})();
+}());
+
+(function type1(){
+    if(count1 === texts1.length){
+        count1 = 0;
+    } else{
+        currentText1 = texts1[count1];
+        index1++;
+        letter1 = currentText1.slice(0,index1);
+        typeWriter1.textContent = letter1;
+        if(letter1.length === currentText1.length){
+            count1++;
+            index1 = 0;
+        }
+    }
+    setTimeout(type1,200);
+}());
+
 
 backgroundDivs = Array.from(backgroundDivs);
 
